f2e079aa14d1dd5fb13092387251dbbdb88d35bb
Chaos Monkey <chaos.monkey@hotmail.com>
2019-12-11 22:06:48 -0600

    Created new solution

---
+++ src/Blogn/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blogn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
