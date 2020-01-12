e0869a5573dcaa98097e6b4b30ac3a5f61d6a782
Chaos Monkey <chaos.monkey@hotmail.com>
2020-01-12 16:42:30 -0600

    UI Clean Up

--- src/Blogn/ViewModels/ChangePasswordModel.cs
+++ src/Blogn/ViewModels/ChangePasswordModel.cs
@@ -7,17 +7,17 @@ namespace Blogn.ViewModels
         [Required]
         [DataType(DataType.Password)]
         [StringLength(256, MinimumLength = 8)]
-        [Display(Name="Current Password", Prompt = "Current Password")]
+        [Display(Name="Current Password")]
         public string CurrentPassword { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [StringLength(256, MinimumLength = 8)]
-        [Display(Name = "New Password", Prompt = "New Password")]
+        [Display(Name = "New Password")]
         public string NewPassword { get; set; }
         [Required]
         [DataType(DataType.Password)]
         [StringLength(256, MinimumLength = 8)]
-        [Display(Name = "Confirm Password", Prompt = "Confirm Password")]
+        [Display(Name = "Confirm Password")]
         [Compare("NewPassword")]
         public string ConfirmPassword { get; set; }
     }
--- src/Blogn/ViewModels/ForgotPasswordModel.cs
+++ src/Blogn/ViewModels/ForgotPasswordModel.cs
@@ -7,6 +7,7 @@ namespace Blogn.ViewModels
         [Required]
         [DataType(DataType.EmailAddress)]
         [StringLength(384)]
+        [Display(Name = "Email Address", Prompt = "Email Address")]
         public string Email { get; set; }
     }
 }
